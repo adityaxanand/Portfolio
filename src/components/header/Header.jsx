8685945526cb5f2133b9245d011210ece0ed6c32
Aditya <adityaanandx@gmail.com>
2024-02-12 19:26:30 +0530

    header commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./header.css";
+
+
+const Header = () => {
+  return (
+    <header className="header">
+        <nav className="nav container"
+    </header>
+  )
+}
+
+export default Header
